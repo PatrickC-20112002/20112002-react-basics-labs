b735555527c92719cf6ddfb3cecb416b59d2a3c9
PatrickC-20112002 <20112002@setu.ie>
2026-09-18 10:54:18 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,9 +1,10 @@
 const Task = (props) => {
-    
+
     return (
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
+            <p className="priority">Priority: {props.priority}</p>
             <p className="description">{props.description}</p>
         </div>
     )
