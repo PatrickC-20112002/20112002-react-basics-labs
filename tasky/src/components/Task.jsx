14f5ef2c7f5090bc6fcdebf73f94cb6c8e0a6d17
PatrickC-20112002 <20112002@setu.ie>
2026-09-18 14:39:47 +0100

    Mark task as done + Delete a task

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,11 +1,28 @@
 const Task = (props) => {
 
     return (
-        <div className="card">
+        <div
+            className="card"
+            style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}
+        >
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="priority">Priority: {props.priority}</p>
             <p className="description">{props.description}</p>
+
+            <button
+                onClick={props.markDone}
+                className="doneButton"
+            >
+                Done
+            </button>
+
+            <button
+                className="deleteButton"
+                onClick={props.deleteTask}
+            >
+                Delete
+            </button>
         </div>
     )
 }
