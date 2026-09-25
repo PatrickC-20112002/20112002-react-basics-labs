de7576a8483becd68848a12076c3514a366a84ec
PatrickC-20112002 <20112002@setu.ie>
2026-09-25 15:05:42 +0100

    Tasky Lab 3 done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 import Task from './components/Task';
 import AddTaskForm from './components/Form';
 import { v4 as uuidv4 } from 'uuid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function App() {
 
@@ -100,25 +103,74 @@ function App() {
   return (
     <div className="container">
 
-      <h1>Tasky</h1>
+      {/* App Header */}
+      <Container component="main">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          gutterBottom
+          sx={{
+            backgroundColor: 'gray',
+            textAlign: 'center',
+            color: 'white',
+            padding: '20px',
+            margin: '20px 0 40px 0',
+            borderRadius: '4px'
+          }}
+        >
+          Tasky
+        </Typography>
+      </Container>
+      {/* End App Header */}
 
-      {taskState.tasks.map((task, index) => (
-        <Task
-          title={task.title}
-          description={task.description}
-          deadline={task.deadline}
-          priority={task.priority}
-          key={task.id}
-          done={task.done}
-          markDone={() => doneHandler(index)}
-          deleteTask={() => deleteHandler(index)}
-        />
-      ))}
+      {/* Task Card Grid */}
+      <Container maxWidth="md" component="main">
+        <Grid
+          container
+          spacing={5}
+          sx={{
+            justifyContent: "center"
+          }}
+        >
+          {taskState.tasks.map((task, index) => (
+            <Task
+              title={task.title}
+              description={task.description}
+              deadline={task.deadline}
+              priority={task.priority}
+              done={task.done}
+              key={task.id}
+              markDone={() => doneHandler(index)}
+              deleteTask={() => deleteHandler(index)}
+            />
+          ))}
+        </Grid>
+      </Container>
+      {/* End Task Card Grid */}
 
-      <AddTaskForm 
-        submit={formSubmitHandler}
-        change={formChangeHandler}
-      />
+      {/* Footer - Add Task Form */}
+      <Container
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+          my: 6,
+          py: 6,
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            justifyContent: "center"
+          }}
+        >
+          <AddTaskForm
+            submit={formSubmitHandler}
+            change={formChangeHandler}
+          />
+        </Grid>
+      </Container>
+      {/* End Footer */}
 
     </div>
   );
